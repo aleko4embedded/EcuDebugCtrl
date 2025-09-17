d538f1c9af614788f14275c4ed221aab8a3282f1
Oleksandr Kotenkov <ok@aleko-embedded.com>
2025-09-17 09:03:04 +0200

    added ssl stuff but seen doesn't work yet

--- create_update_daemon.sh
+++ create_update_daemon.sh
@@ -1,5 +1,13 @@
-!#/usr/bin/bash
+#!/usr/bin/bash
 
+#create cert folder
+mkdir -p /home/aetech/raspi-http/EcuDebugCtrl/certs
+cd /home/aetech/raspi-http/EcuDebugCtrl/certs
+#generate cert
+#openssl ecparam -genkey -name prime256v1 -noout -out key.pem
+#openssl req -new -x509 -key key.pem -out cert.pem -days 365
+
+cd ..
 sudo cp ecudebugcontrol.service /etc/systemd/system/
 sudo systemctl daemon-reload 
 sudo systemctl enable ecudebugcontrol.service 
